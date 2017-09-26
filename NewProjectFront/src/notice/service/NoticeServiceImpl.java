e4a5b9bb1aa54952af9df787e8f4b15562abd530
kosmo_12 <kosmo_12@kosmo_12-PC>
2017-09-26 18:05:48 +0900

    공지 페이징

--- NewProjectFront/src/notice/service/NoticeServiceImpl.java
+++ NewProjectFront/src/notice/service/NoticeServiceImpl.java
@@ -1,6 +1,8 @@
 package notice.service;
 
 import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 import notice.service.impl.NoticeDao;
@@ -12,9 +14,20 @@ public class NoticeServiceImpl implements NoticeService{
 	public NoticeDao dao;
 
 	@Override
-	public List<Noti_ModelDto> selectNoti_ModelList() throws Exception {
+	public List<Noti_ModelDto> selectNoti_ModelList(Map map) throws Exception {
 		
-		return dao.selectNoti_ModelList();
+		return dao.selectNoti_ModelList(map);
+	}
+
+	@Override
+	public Noti_ModelDto selectOne(Map map) throws Exception {
+		
+		return dao.selectOne(map);
+	}
+
+	@Override
+	public int getTotalCount(Map map) throws Exception {
+		return dao.getTotalCount(map);
 	}
 	
 	
